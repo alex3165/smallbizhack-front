f47bbe4aca2902a923b0789d0cb656e10b162d7d
CoreFloDev <CoreFloDev@users.noreply.github.com>
2017-03-04 15:14:47 +0000

    Add login button

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router'
 
 export default class Login extends Component {
+
   render() {
     return (
       <div>
-        Test
+        <button class="login" onClick={() => browserHistory.push("/merchants")} >Login</button>
       </div>
     );
   }
